c141b933d803a38b77c9f9a6027c6a8e941daaad
Almazov-Artyom <artemalmazov3@gmail.com>
2025-04-26 21:36:11 +0300

    add JwtCacheService

---
+++ src/main/java/ru/almaz/dailycalorieintake/service/JwtCacheService.java
@@ -0,0 +1,10 @@
+package ru.almaz.dailycalorieintake.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtCacheService {
+
+}
